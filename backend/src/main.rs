904262c8cccbc4b0011dcb4c374c8d0ac80da044
Awesxme <manasvi.d007@gmail.com>
2026-06-30 18:17:11 +0000

    init

---
+++ backend/src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+
+#[get("/")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("Pong")
+}
+
+async fn manual_ping() -> impl Responder {
+    HttpResponse::Ok().body("Heyy")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+            .route("/ping", web::get().to(manual_ping))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
