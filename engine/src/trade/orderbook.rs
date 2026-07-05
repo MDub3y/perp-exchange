11756ecc029fb7a27d79653cc4e014f76471ec74
Awesxme <manasvi.d007@gmail.com>
2026-07-05 13:15:35 +0000

    added method declarations

--- engine/src/trade/orderbook.rs
+++ engine/src/trade/orderbook.rs
@@ -1,4 +1,7 @@
-use std::collections::{BTreeMap, VecDeque};
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, VecDeque},
+};
 
 use chrono::Utc;
 use rust_decimal::Decimal;
@@ -20,18 +23,29 @@ pub enum OrderType {
     MARKET,
 }
 
+pub enum OrderStatus {
+    Pending,
+    Filled,
+    PartiallyFilled,
+    Cancelled,
+}
+
 pub struct Order {
-    pub user_id: Uuid,
-    pub market: Market,
     pub order_id: Uuid,
     pub price: Decimal,
     pub quantity: Decimal,
+}
+
+pub struct OrderPayload {
+    pub user_id: Uuid,
+    pub market: Market,
+    pub order: Order,
     pub order_side: OrderSide,
     pub order_type: OrderType,
     pub timestamp: i64,
 }
 
-impl Order {
+impl OrderPayload {
     pub fn new(
         user_id: Uuid,
         market: Market,
@@ -42,12 +56,15 @@ impl Order {
     ) -> Self {
         let order_id = Uuid::new_v4();
         let timestamp = Utc::now().timestamp_millis();
+
         Self {
             user_id,
             market,
-            order_id,
-            price,
-            quantity,
+            order: Order {
+                order_id,
+                price,
+                quantity,
+            },
             order_side,
             order_type,
             timestamp,
@@ -55,8 +72,93 @@ impl Order {
     }
 }
 
+pub struct Fill {
+    pub price: Decimal,
+    pub quantity: Decimal,
+    pub user_id: Uuid,
+    pub order_id: Uuid,
+    pub matching_user_id: Uuid,
+}
+
+pub struct ProcessOrderResult {
+    pub fills: Vec<Fill>,
+    pub executed_quantity: Decimal,
+}
+
 pub struct Orderbook {
-    bids: BTreeMap<Decimal, VecDeque<Order>>,
+    bids: BTreeMap<Reverse<Decimal>, VecDeque<Order>>,
     asks: BTreeMap<Decimal, VecDeque<Order>>,
     market: Market,
+}
+
+impl Orderbook {
+    pub fn new(market: Market) -> Self {
+        let mut bids = BTreeMap::new();
+        let mut asks = BTreeMap::new();
+
+        Self { bids, asks, market }
+    }
+
+    pub fn get_bids(&self) -> &BTreeMap<Reverse<Decimal>, VecDeque<Order>> {
+        &self.bids
+    }
+
+    pub fn process_order(&mut self, payload: OrderPayload) -> Result<(), String> {
+        match payload.order_type {
+            OrderType::LIMIT => {
+                match payload.order_side {
+                    // TODO: matching logic if awailable
+                    OrderSide::BUY => self
+                        .bids
+                        .entry(Reverse(payload.order.price))
+                        .or_default()
+                        .push_back(payload.order),
+                    OrderSide::SELL => self
+                        .asks
+                        .entry(payload.order.price)
+                        .or_default()
+                        .push_back(payload.order),
+                }
+            }
+            OrderType::MARKET => {
+                match payload.order_side {
+                    OrderSide::BUY => {
+                        // walk the asks side and fill
+                    }
+                    OrderSide::SELL => {
+                        // walk the bids side and fill
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn match_asks() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn match_bids() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn get_depth() -> Result<(), String> {
+        // asks + bids
+        Ok(())
+    }
+
+    pub fn cancel_order() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn cancel_all_orders() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn get_open_order() -> Result<(), String> {
+        // not sure if this needs to list all the orders, as how will the user query using the order_id
+        // but search by user id is possible
+        Ok(())
+    }
 }
